898bb00e026e4a25d97e55cad936978f5888710e
Martin Coder <martincoder1@gmail.com>
2023-05-24 15:04:41 -0700

    create nav bar, request tables

--- safewalk_admin/lib/main.dart
+++ safewalk_admin/lib/main.dart
@@ -9,13 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return MaterialApp(
-      title: 'SafeWalk Admin',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(),
+      // title: 'SafeWalk Admin',
+      color: theme.colorScheme.primary,
+      home: const MyHomePage(),
     );
   }
 }
@@ -25,62 +24,197 @@ class MyApp extends StatelessWidget {
 // }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // padding:
-      color: Colors.white,
-      child: DataTable(
-        // border: TableBorder.all(color: Colors.black),
-        columns: [
-          DataColumn(
-            label: Text('Time'),
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.directions_walk),
+                  label: Text('Walk Requests'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.people),
+                  label: Text('Walkers'),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print('selected: $value');
+              },
+            ),
           ),
-          DataColumn(
-            label: Text('Name'),
-          ),
-          DataColumn(
-            label: Text('Current Location'),
-          ),
-          DataColumn(
-            label: Text('Destination'),
-          ),
-          DataColumn(label: Text('Assign')),
-          DataColumn(
-            label: Text('Delete'),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: const WalkRequestsPage(),
+            ),
           ),
         ],
-        rows: [
-          DataRow(
-            cells: [
-              DataCell(Text('00:00')),
-              DataCell(Text('Helen Mirren')),
-              DataCell(Text('1234 Bellomy St')),
-              DataCell(Text('Graham')),
-              DataCell(
-                IconButton(
-                  icon: const Icon(Icons.person_add),
-                  onPressed: () {},
+      ),
+    );
+  }
+}
+
+class WalkRequestsPage extends StatelessWidget {
+  const WalkRequestsPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 45, bottom: 10, top: 5),
+                  child: Text(
+                    'Walk Requests',
+                    style: TextStyle(
+                      // Theme.of(context).textTheme.displayMedium,
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontSize: 40,
+                    ),
+                  ),
                 ),
               ),
-              DataCell(
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {},
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(0),
+                  decoration: BoxDecoration(
+                    // border: Border.all(color: Colors.black),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: DataTable(
+                      headingRowColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.white),
+                      dataRowColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.white),
+                      columns: const [
+                        DataColumn(
+                          label: Text('Time'),
+                        ),
+                        DataColumn(
+                          label: Text('Name'),
+                        ),
+                        DataColumn(
+                          label: Text('Current Location'),
+                        ),
+                        DataColumn(
+                          label: Text('Destination'),
+                        ),
+                        DataColumn(
+                          label: Text('Assign'),
+                        ),
+                        DataColumn(
+                          label: Text('Delete'),
+                        ),
+                      ],
+                      rows: [
+                        DataRow(
+                          cells: [
+                            const DataCell(Text('00:00')),
+                            const DataCell(Text('Helen Mirren')),
+                            const DataCell(Text('1234 Bellomy St')),
+                            const DataCell(Text('Graham')),
+                            DataCell(
+                              IconButton(
+                                icon: const Icon(Icons.person_add),
+                                onPressed: () {},
+                              ),
+                            ),
+                            DataCell(
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () {},
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    // border: Border.all(color: Colors.black),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: DataTable(
+                      headingRowColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.white),
+                      dataRowColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.white),
+                      columns: const [
+                        DataColumn(
+                          label: Text('Time'),
+                        ),
+                        DataColumn(
+                          label: Text('Name'),
+                        ),
+                        DataColumn(
+                          label: Text('Current Location'),
+                        ),
+                        DataColumn(
+                          label: Text('Destination'),
+                        ),
+                        DataColumn(
+                          label: Text('Assign'),
+                        ),
+                        DataColumn(
+                          label: Text('Delete'),
+                        ),
+                      ],
+                      rows: [
+                        DataRow(
+                          cells: [
+                            const DataCell(Text('00:00')),
+                            const DataCell(Text('Helen Mirren')),
+                            const DataCell(Text('1234 Bellomy St')),
+                            const DataCell(Text('Graham')),
+                            DataCell(
+                              IconButton(
+                                icon: const Icon(Icons.person_add),
+                                onPressed: () {},
+                              ),
+                            ),
+                            DataCell(
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () {},
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-        ],
-        // TableRow(children: [
-        //   Text('Time'),
-        //   Text('Name'),
-        //   Text('Current Location'),
-        //   Text('Destination'),
-        //   Text('Assign'),
-        // //   Text('Delete'),
-        //   // ])
-        // ],
+        ),
       ),
     );
   }
